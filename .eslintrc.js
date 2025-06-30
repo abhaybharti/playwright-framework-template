0af246a6e52d692e1c0378d698a115874a8a15e2
Abhay Bharti <abhaybharti@users.noreply.github.com>
2025-06-30 10:23:45 +0530

    added code quality rultes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  plugins: ["@typescript-eslint", "import", "prettier", "playwright"],
+  plugins: ["@typescript-eslint", "import", "prettier", "playwright", "sonarjs","unicorn","jsdoc"],
   extends: [
     "prettier",
     "plugin:@typescript-eslint/recommended",
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:import/recommended",
     "plugin:playwright/playwright-test",
     "plugin:prettier/recommended",
+    "plugin:unicorn/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -33,7 +34,24 @@ module.exports = {
       "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
       "newlines-between": "always",
       "alphabetize": { "order": "asc" }
-    }]
+    },
+    ],
+    "sonarjs/cognitive-complexity": ["error", 15], // Example: Limit function complexity
+    "sonarjs/no-duplicate-string": "error", // Prevent duplicate strings,
+    "unicorn/prevent-abbreviations": "off", // Customize: Disable abbreviation rule if too strict
+    "unicorn/filename-case": [
+      "error",
+      { "case": "kebabCase" } // Enforce kebab-case for filenames
+    ],
+    "jsdoc/require-jsdoc": [
+      "error",
+      {
+        "publicOnly": true, // Require JSDoc for exported/public functions
+        "require": { "FunctionDeclaration": true, "ArrowFunctionExpression": false }
+      }
+    ],
+    "jsdoc/require-param-description": "off", // Customize: Allow params without descriptions
+    "jsdoc/require-returns": "error"
   },
   overrides: [
     {
